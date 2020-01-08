b110dd13326cecdc326219e6db5c20c0c73ed243
zcc <zhangcongcong.work@foxmail.com>
2020-01-08 19:33:48 +0800

    lengthOfLongestSubstring

--- Tests/algorithm/lengthOfLongestSubstring.js
+++ Tests/algorithm/lengthOfLongestSubstring.js
@@ -1,27 +1,28 @@
+/*
+ * @LastEditors  : zcc
+ * @Date: 2020-01-08 19:08:20
+ * @Description:  
+ */
 /**
  * @param {string} s
  * @return {number}
  * @description 最长回文子串必须连续，而最长回文子序列可以在原字符串中不连续
  */
-function ListNode(val) {
-  this.val = val;
-  this.next = null;
-}
+
 var lengthOfLongestSubstring = function (s) {
   let arrStr = Array.from(s)
   if (arrStr.length === 1) return 1
-  let arr = []
   let str = ''
   let len = ''
   for (let i = 0; i < arrStr.length; i++) {
     const element = arrStr[i];
-    if (str.indexOf(element) === -1) {
+    const index = str.indexOf(element)
+    if (index === -1) {
       str += element
-      len = str.length > len ? str.length : len
     } else {
-      arr.push(str)
-      str = element
+      str = str.slice(index+1)+element
     }
+    len = str.length > len ? str.length : len
   }
   return len
 }
