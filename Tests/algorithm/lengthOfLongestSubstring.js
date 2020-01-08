385f48a2641b6f7c22348ba4efeff8f94810ef27
zcc <zhangcongcong.work@foxmail.com>
2020-01-08 18:05:54 +0800

    error

--- Tests/algorithm/lengthOfLongestSubstring.js
+++ Tests/algorithm/lengthOfLongestSubstring.js
@@ -1,9 +1,35 @@
 /**
  * @param {string} s
  * @return {number}
+ * @description 最长回文子串必须连续，而最长回文子序列可以在原字符串中不连续
  */
-var lengthOfLongestSubstring = function(s) {
-    const b = Array.from(s)
-    console.log(b)
-};
-lengthOfLongestSubstring("abcabcbb")
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+var lengthOfLongestSubstring = function (s) {
+  let arrStr = Array.from(s)
+  if (arrStr.length === 1) return 1
+  let arr = []
+  let str = ''
+  let len = ''
+  for (let i = 0; i < arrStr.length; i++) {
+    const element = arrStr[i];
+    if (str.indexOf(element) === -1) {
+      str += element
+      len = str.length > len ? str.length : len
+    } else {
+      arr.push(str)
+      str = element
+    }
+  }
+  return len
+}
+const a = lengthOfLongestSubstring('abcabcbb')
+console.log(a)
+const b = lengthOfLongestSubstring('bbbbb')
+console.log(b)
+const c = lengthOfLongestSubstring('pwwkew')
+console.log(c)
+const d = lengthOfLongestSubstring('dvdf')
+console.log(d)
